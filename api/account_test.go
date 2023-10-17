d5e1a7204f0b8ca8b8d62ade79051b4f698969b4
pranaliv <pranalivv1012@gmail.com>
2023-10-17 11:28:36 +0530

    updated go mod

---
+++ api/account_test.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/pranali1012/simple_bank/db"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+
+}
+
+func RandomAccount() db.Account {
+	return db.Account{}
+}
--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/pranali1012/simple_bank/api"
 	db "github.com/pranali1012/simple_bank/db/sqlc"
+	"github.com/pranali1012/simple_bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
 	//Connecting to the database
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to the database: ", err)
 	}
@@ -28,11 +28,11 @@ func main() {
 	// }
 
 	store := db.NewStore(conn)
-	server := api.newServer(store)
+	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
-
+		log.Fatal("cannot start server:", err)
 	}
 
 	//Creating an instance of a Queries struct from sqlc package
